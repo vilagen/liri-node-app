06739522c31e36797a09aae62717efed9853604a
vilagen <vilagen@yahoo.com>
2019-06-21 12:12:03 -0400

    Updating files like .gitignore and liri.js

---
+++ liri.js
@@ -0,0 +1,13 @@
+require('dotenv').config();
+
+var keys = require("./keys.js")
+
+var spofity = new Spotify(keys.spotify);
+
+// concert-this
+
+// spofity-this-song
+
+// movie-this
+
+// do-what-it-says
