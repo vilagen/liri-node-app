c234b91a411b3b6ad4fb8107704b40db81b132f7
vilagen <vilagen@yahoo.com>
2019-06-22 09:34:50 -0400

    added some new files.

--- liri.js
+++ liri.js
@@ -1,10 +1,15 @@
 require('dotenv').config();
 
 var keys = require("./keys.js")
+var axios = require("axios")
 
 var spofity = new Spotify(keys.spotify);
 
-// concert-this
+// concert-this: "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"
+
+artist = process.argv[2]
+
+axios.get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp")
 
 // spofity-this-song
 
