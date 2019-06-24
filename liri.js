90531e673e86b400c82911ec935d153e2ecb8fa0
vilagen <vilagen@yahoo.com>
2019-06-24 07:20:33 -0400

    Made switch command, but omdb api now not working.

--- liri.js
+++ liri.js
@@ -4,116 +4,136 @@ require("dotenv").config();
 var keys = require("./keys.js")
 var axios = require("axios")
 
+var command = process.argv[2]
 
+switch(command) {
 
+case "concert-this":
+    concert();
+    break;
+
+case "spotify-this-song":
+    spotify();
+    break;
+
+case "movie-this":
+    omdb();
+    break;
+
+case "do-what-it-says":
+    random();
+    break;
+}
 
 // use a switch set to process arg 2 for what if statements for each command. 
 // then make arg 3 follow what the user inputs.
 
+function concert(){
+artist = process.argv.slice(3).join(" ")
+queryURL = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"
 
-
-// concert-this: "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"
-
-// artist = process.argv[2]
-// queryURL = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"
-
-// axios.get(queryURL).then(
-//     function(response) {
-//         events = response.data
-//         for(var i = 0; i < events.length; i++) {
-//             console.log(" ---------- " +
-//                         "\n" + events[i].venue.name +
-//                         "\n" + events[i].venue.city +
-//                         "\n" + events[i].datetime)
-//         }
-// }).catch(function(error) {
-//     if (error.response) {
-//      console.log(error)
-//     }
-//     else if (error.request) {
-//     // request was made but no response
-//     console.log(error.request)
-//     }
-//     else {
-//     // for anything else that may have caused an error.
-//     console.log("Error", error.message)
-//     }
-// })
+axios.get(queryURL).then(
+    function(response) {
+        events = response.data
+        for(var i = 0; i < events.length; i++) {
+            console.log(" ---------- " +
+                        "\n Venue: " + events[i].venue.name +
+                        "\n Venue City: " + events[i].venue.city +
+                        "\n Datetime: " + events[i].datetime)
+        }
+}).catch(function(error) {
+    if (error.response) {
+     console.log(error)
+    }
+    else if (error.request) {
+    // request was made but no response
+    console.log(error.request)
+    }
+    else {
+    // for anything else that may have caused an error.
+    console.log("Error", error.message)
+    }
+})
+}
 
 // spofity-this-song
 
-// var Spotify = require('node-spotify-api')
-// var song = process.argv.slice(2).join(" ")
+function spotify() {
+var Spotify = require('node-spotify-api')
+var song = process.argv.slice(3).join(" ")
 
-// var trackURL = "https://api.spotify.com/v1/search?q=" + song + "&type=track&limit=3"
-
-
-// var spotify = new Spotify(keys.spotify);
+var trackURL = "https://api.spotify.com/v1/search?q=" + song + "&type=track&limit=3"
 
 
-// spotify.request(trackURL)
-// .then(function(data) {
-//         console.log(data.tracks.items[0].artists[0].name)
-//         trackInfo = data.tracks.items
-//         for(var i = 0; i < trackInfo.length; i++) {
-//             console.log(" ------------- " +
-//             "\n Artist: " + data.tracks.items[i].artists[0].name +
-//             "\n Track: " + data.tracks.items[i].name +
-//             "\n Spotify Link: " + data.tracks.items[i].external_urls.spotify +
-//             "\n Album: " + data.tracks.items[i].album.name)
-//         }
-// })
-// .catch(function(err){
-//     console.error('Error occured: ' + err)
-// })
+var spotify = new Spotify(keys.spotify);
+
+
+spotify.request(trackURL)
+.then(function(data) {
+        console.log(data.tracks.items[0].artists[0].name)
+        trackInfo = data.tracks.items
+        for(var i = 0; i < trackInfo.length; i++) {
+            console.log(" ------------- " +
+            "\n Artist: " + data.tracks.items[i].artists[0].name +
+            "\n Track: " + data.tracks.items[i].name +
+            "\n Spotify Link: " + data.tracks.items[i].external_urls.spotify +
+            "\n Album: " + data.tracks.items[i].album.name)
+        }
+})
+.catch(function(err){
+    console.error('Error occured: ' + err)
+})
+}
 
 // movie-this
 
-// var omdb = require("omdb");
+var omdb = require("omdb");
 
-// movie = process.argv.slice(2).join(" ")
+function omdb() {
+movie = process.argv.slice(3).join(" ")
 
-// var movieSearch = "https://www.omdbapi.com/?t=" + movie + "&apikey=5074847d"
+var movieSearch = "https://www.omdbapi.com/?t=" + movie + "&apikey=5074847d"
 
-// axios.get(movieSearch).then(
-//     function(response){
-//         if(!response.data.Ratings) {
-//             console.log(" ----------- " +
-//             "\n Title: " + response.data.Title +
-//             "\n Year of Release: " + response.data.Year +
-//             "\n IMDB Rating: " + response.data.imdbRating +
-//             "\n No Rotten Tomatoes Score" +
-//             "\n Country: " + response.data.Country +
-//             "\n Langauge: " + response.data.Language +
-//             "\n Plot: " + response.data.Plot +
-//             "\n Actors: " + response.data.Actors)
-//         }
+axios.get(movieSearch).then(
+    function(response){
+        // if(!response.data.Ratings) {
+        //     console.log(" ----------- " +
+        //     "\n Title: " + response.data.Title +
+        //     "\n Year of Release: " + response.data.Year +
+        //     "\n IMDB Rating: " + response.data.imdbRating +
+        //     "\n No Rotten Tomatoes Score" +
+        //     "\n Country: " + response.data.Country +
+        //     "\n Langauge: " + response.data.Language +
+        //     "\n Plot: " + response.data.Plot +
+        //     "\n Actors: " + response.data.Actors)
+        // }
 
-//         else{
-//             console.log(" ----------- " +
-//             "\n Title: " + response.data.Title +
-//             "\n Year of Release: " + response.data.Year +
-//             "\n IMDB Rating: " + response.data.imdbRating +
-//             "\n " + response.data.Ratings[1].Source + ": " + response.data.Ratings[1].Value +
-//             "\n Country: " + response.data.Country +
-//             "\n Langauge: " + response.data.Language +
-//             "\n Plot: " + response.data.Plot +
-//             "\n Actors: " + response.data.Actors)
-//         }
-// })
-//     .catch(function(error) {
-//         if (error.response) {
-//         console.log(error)
-//         }
-//         else if (error.request) {
-//         // request was made but no response
-//         console.log(error.request)
-//         }
-//         else {
-//         // for anything else that may have caused an error.
-//         console.log("Error", error.message)
-//         }
-// })
+        // else{
+            console.log(" ----------- " +
+            "\n Title: " + response.data.Title +
+            "\n Year of Release: " + response.data.Year +
+            "\n IMDB Rating: " + response.data.imdbRating +
+            "\n " + response.data.Ratings[1].Source + ": " + response.data.Ratings[1].Value +
+            "\n Country: " + response.data.Country +
+            "\n Langauge: " + response.data.Language +
+            "\n Plot: " + response.data.Plot +
+            "\n Actors: " + response.data.Actors)
+        // }
+})
+    .catch(function(error) {
+        if (error.response) {
+        console.log(error)
+        }
+        else if (error.request) {
+        // request was made but no response
+        console.log(error.request)
+        }
+        else {
+        // for anything else that may have caused an error.
+        console.log("Error", error.message)
+        }
+})
+}
 
 // do-what-it-says
 
